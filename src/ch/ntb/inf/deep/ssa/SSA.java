7b6436cf4ad70454328f3fd0605a52cc05aa0dd8
Roger Millischer <roger.millischer@ntb.ch>
2010-04-16 08:18:53 +0000

    Add SSA and changes on SSANode

---
+++ src/ch/ntb/inf/deep/ssa/SSA.java
@@ -0,0 +1,12 @@
+package ch.ntb.inf.deep.ssa;
+
+import ch.ntb.inf.deep.cfg.CFG;
+
+
+public class SSA {
+	
+	public SSA(CFG cfg) {
+		
+	}
+
+}
--- src/ch/ntb/inf/deep/ssa/SSANode.java
+++ src/ch/ntb/inf/deep/ssa/SSANode.java
@@ -4,10 +4,24 @@ import ch.ntb.inf.deep.cfg.CFGNode;
 import ch.ntb.inf.deep.ssa.instruction.SSAInstruction;
 
 public class SSANode extends CFGNode {
-	public SSAValue entrySet;
-	public SSAValue exitSet;
+	private int nofInstr = 4;
+	private int nofPhiFunc = 2;
+	public SSAValue entrySet[];
+	public SSAValue exitSet[];
 	public SSAValue operandStack[];
 	public SSAInstruction instructions[];
 	public PhiFunction phiFunctions[];	
 
+
+
+	public SSANode(int maxLocals, int maxStack){
+		super();
+		entrySet = new SSAValue[maxLocals];
+		exitSet = new SSAValue[maxLocals];
+		operandStack = new SSAValue[maxStack];
+		instructions = new SSAInstruction[nofInstr];
+		phiFunctions = new PhiFunction[nofPhiFunc];
+		
+	}
+	
 }
