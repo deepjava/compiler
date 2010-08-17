4e2923e86f98f940216ce7cfb7c2968ed4aef5a6
Roger Millischer <roger.millischer@ntb.ch>
2010-08-17 11:02:13 +0000

    add SSA Testcase stubs

---
+++ jUnitTest/ch/ntb/inf/deep/ssa/SSA05.java
@@ -0,0 +1,35 @@
+package ch.ntb.inf.deep.ssa;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ch.ntb.inf.deep.classItems.Class;
+import ch.ntb.inf.deep.classItems.IClassFileConsts;
+import ch.ntb.inf.deep.classItems.Type;
+
+public class SSA05 extends TestSSA {
+
+	@BeforeClass
+	public static void setUp() {
+		String[] rootClassNames = new String[] { "ch/ntb/inf/deep/testClasses/T05Returns" };
+		try {
+			Class.buildSystem(rootClassNames, (1 << IClassFileConsts.atxCode)
+					| (1 << IClassFileConsts.atxLocalVariableTable)
+					| (1 << IClassFileConsts.atxLineNumberTable)
+					| (1 << IClassFileConsts.atxExceptions));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (Type.nofRootClasses > 0) {
+			createSSA(Type.rootClasses[0]);
+		}
+	}
+
+	@Test
+	public void testConstructor() {
+
+	}
+}
