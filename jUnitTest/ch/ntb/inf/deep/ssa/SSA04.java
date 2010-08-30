6bcde218c529aeadadf78bb23c2e28f3b226deb8
Roger Millischer <roger.millischer@ntb.ch>
2010-08-30 15:05:00 +0000

    specified test results

--- jUnitTest/ch/ntb/inf/deep/ssa/SSA04.java
+++ jUnitTest/ch/ntb/inf/deep/ssa/SSA04.java
@@ -30,6 +30,98 @@ public class SSA04 extends TestSSA {
 
 	@Test
 	public void testConstructor() {
-
+		SSANode[] nodes = getAndTestSSA(0, 1, 0);
+		testNode(nodes[0], 2, 0, 2);
+	}
+	
+	@Test
+	public void testDoWhile1(){
+		SSANode[] nodes = getAndTestSSA(1, 3, 1);
+		testNode(nodes[0], 1, 0, 4);
+		testNode(nodes[1], 3, 1, 4);
+		testNode(nodes[2], 2, 0, 4);
+	}
+	
+	@Test
+	public void testDoWhileIf1(){
+		SSANode[] nodes = getAndTestSSA(2, 13, 1);
+		testNode(nodes[0], 2, 0, 6);
+		testNode(nodes[1], 4, 2, 6);
+		testNode(nodes[2], 1, 0, 6);
+		testNode(nodes[3], 1, 0, 6);
+		testNode(nodes[4], 1, 1, 6);
+		testNode(nodes[5], 1, 0, 6);
+		testNode(nodes[6], 1, 0, 6);
+		testNode(nodes[7], 0, 1, 6);
+		testNode(nodes[8], 0, 0, 6);
+		testNode(nodes[9], 0, 0, 6);
+		testNode(nodes[10], 1, 0, 6);
+		testNode(nodes[11], 1, 0, 6);
+		testNode(nodes[12], 0, 1, 6);
+	}
+	
+	@Test
+	public void testWhile1(){
+		SSANode[] nodes = getAndTestSSA(3, 4, 1);
+		testNode(nodes[0], 1, 0, 3);
+		testNode(nodes[1], 2, 0, 3);
+		testNode(nodes[2], 1, 1, 3);
+		testNode(nodes[3], 0, 0, 3);
+	}
+	
+	@Test
+	public void testWhileTrue(){
+		SSANode[] nodes = getAndTestSSA(4, 2, 1);
+		testNode(nodes[0], 1, 0, 4);
+		testNode(nodes[1], 2, 0, 4);
+	}
+	
+	@Test
+	public void testWhileTrueBreak(){
+		SSANode[] nodes = getAndTestSSA(5, 1, 0);
+		testNode(nodes[0], 3, 0, 4);
+	}
+	
+	@Test
+	public void testWhileMultiCond(){
+		SSANode[] nodes = getAndTestSSA(6, 5, 1);
+		testNode(nodes[0], 1, 0, 3);
+		testNode(nodes[1], 2, 0, 3);
+		testNode(nodes[2], 1, 1, 3);
+		testNode(nodes[3], 0, 0, 3);
+		testNode(nodes[4], 0, 0, 3);
+	}
+	
+	@Test
+	public void testFor1(){
+		SSANode[] nodes = getAndTestSSA(7, 4, 1);
+		testNode(nodes[0], 2, 0, 4);
+		testNode(nodes[1], 4, 0, 4);
+		testNode(nodes[2], 1, 2, 4);
+		testNode(nodes[3], 0, 0, 4);
+	}
+	
+	@Test
+	public void testForWhile(){
+		SSANode[] nodes = getAndTestSSA(8, 6, 2);
+		testNode(nodes[0], 2, 0, 5);
+		testNode(nodes[1], 2, 0, 5);
+		testNode(nodes[2], 1, 1, 5);
+		testNode(nodes[3], 2, 0, 5);
+		testNode(nodes[4], 0, 1, 5);
+		testNode(nodes[5], 0, 0, 5);
+	}
+	
+	@Test
+	public void testForIfWhile(){
+		SSANode[] nodes =getAndTestSSA(9, 8, 2);
+		testNode(nodes[0], 1, 0, 4);
+		testNode(nodes[1], 1, 0, 4);
+		testNode(nodes[2], 0, 0, 4);
+		testNode(nodes[3], 2, 0, 4);
+		testNode(nodes[4], 1, 1, 4);
+		testNode(nodes[5], 2, 2, 4);
+		testNode(nodes[6], 1, 1, 4);
+		testNode(nodes[7], 0, 0, 4);
 	}
 }
