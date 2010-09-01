e809c9bc3e7eea1218b49347c5a7d3dbc6421a39
Roger Millischer <roger.millischer@ntb.ch>
2010-09-01 14:36:41 +0000

    add testresults

--- jUnitTest/ch/ntb/inf/deep/testClasses/T07Arrays.java
+++ jUnitTest/ch/ntb/inf/deep/testClasses/T07Arrays.java
@@ -2,6 +2,29 @@ package ch.ntb.inf.deep.testClasses;
 
 
 public class T07Arrays {
+	
+	public static void emptyIntArray(){
+		int[] a = new int[5];
+	}
+	
+	public static int intArray(int start){
+		int[] b = new int[5];
+		for (int i = 0; i < b.length; i++){
+			b[i]= i + start; 
+		}
+		return b[2];
+	}
+	
+	public static String stringArray(){
+		String[] strings = new String[]{"This", "is", "a", "test", "string"};
+		
+		return strings[0];
+	}
+	
+	public static void objectArray(){
+		Object[] objs = new Object[3];
+		objs[0] = new Object();
+	}
 
 
 }
