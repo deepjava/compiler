a3ae8afe5012531011bedaff51b4af83940b140d
Roger Millischer <roger.millischer@ntb.ch>
2010-09-01 13:57:08 +0000

    add testresults

--- jUnitTest/ch/ntb/inf/deep/ssa/SSA06.java
+++ jUnitTest/ch/ntb/inf/deep/ssa/SSA06.java
@@ -30,6 +30,42 @@ public class SSA06 extends TestSSA {
 
 	@Test
 	public void testConstructor() {
-
+		SSANode[] nodes = getAndTestSSA(0, 1, 0);
+		testNode(nodes[0], 2, 0, 2);
+	}
+	
+	@Test
+	public void testConditionalOperator1() {
+		SSANode[] nodes = getAndTestSSA(1, 11, 0);
+		testNode(nodes[0], 4, 0, 6);
+		testNode(nodes[1], 0, 0, 6);
+		testNode(nodes[2], 0, 0, 6);
+		testNode(nodes[3], 1, 1, 6);
+		testNode(nodes[4], 0, 0, 6);
+		testNode(nodes[5], 0, 0, 6);
+		testNode(nodes[6], 0, 0, 6);
+		testNode(nodes[7], 0, 0, 6);
+		testNode(nodes[8], 0, 0, 6);
+		testNode(nodes[9], 0, 0, 6);
+		testNode(nodes[10], 1, 1, 6);
+	}
+	
+	@Test
+	public void testConditionalOperator2() {
+		SSANode[] nodes = getAndTestSSA(2, 14, 0);
+		testNode(nodes[0], 8, 0, 11);
+		testNode(nodes[1], 0, 0, 11);
+		testNode(nodes[2], 0, 0, 11);
+		testNode(nodes[3], 1, 1, 11);
+		testNode(nodes[4], 0, 0, 11);
+		testNode(nodes[5], 0, 0, 11);
+		testNode(nodes[6], 0, 0, 11);
+		testNode(nodes[7], 0, 0, 11);
+		testNode(nodes[8], 0, 0, 11);
+		testNode(nodes[9], 0, 0, 11);
+		testNode(nodes[10], 1, 1, 11);
+		testNode(nodes[11], 0, 0, 11);
+		testNode(nodes[12], 1, 0, 11);
+		testNode(nodes[13], 1, 0, 11);
 	}
 }
