00dba593dec175904d6b18c61d8d09f792fe5574
Roger Millischer <roger.millischer@ntb.ch>
2010-09-03 13:51:51 +0000

    initial import

---
+++ jUnitTest/ch/ntb/inf/deep/testClasses/T08Calls.java
@@ -0,0 +1,40 @@
+package ch.ntb.inf.deep.testClasses;
+
+public class T08Calls {
+	
+	public static int classMethCall(){
+		int y = 0;
+		int x = classMethod(3);
+		y = 2;
+		
+		return y+x;		
+	}
+	
+	public static int objectMethCall(){
+		T08Calls call = new T08Calls();
+		return call.objectMethod(9);
+	}
+	
+	public static void callToAnotherClass(){
+		T01SimpleMethods.emptyMethodStatic();
+	}
+	
+	
+	/*
+	 * Helper class
+	 */
+	public static int classMethod(int param){
+		int x = 0;
+		for(int i = 0; i < param; i++){
+			x = x+3;
+		}
+		return x;		
+	}
+	/*
+	 * Helper class
+	 */
+	public int objectMethod(int param){
+		return param % 2;		
+	}
+
+}
