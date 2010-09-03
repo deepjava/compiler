160a45dd9c73948cc26509b0143b33b9eb479a3d
Roger Millischer <roger.millischer@ntb.ch>
2010-09-03 13:51:43 +0000

    initial import

---
+++ jUnitTest/ch/ntb/inf/deep/ssa/SSA08.java
@@ -0,0 +1,36 @@
+package ch.ntb.inf.deep.ssa;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import ch.ntb.inf.deep.classItems.Class;
+import ch.ntb.inf.deep.classItems.IClassFileConsts;
+import ch.ntb.inf.deep.classItems.Type;
+
+public class SSA08 extends TestSSA {
+	@BeforeClass
+	public static void setUp() {
+		String[] rootClassNames = new String[] { "ch/ntb/inf/deep/testClasses/T08Calls" };
+		try {
+			Class.buildSystem(rootClassNames, (1 << IClassFileConsts.atxCode)
+					| (1 << IClassFileConsts.atxLocalVariableTable)
+					| (1 << IClassFileConsts.atxLineNumberTable)
+					| (1 << IClassFileConsts.atxExceptions));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		if (Type.nofRootClasses > 0) {
+			createSSA(Type.rootClasses[0]);
+		}
+	}
+
+	@Test
+	public void testConstructor() {
+		SSANode[] nodes = getAndTestSSA(0, 1, 0);
+		testNode(nodes[0], 2, 0, 2);
+	}
+
+}
