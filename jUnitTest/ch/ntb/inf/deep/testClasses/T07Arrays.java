7003f76fd40a70fb9d5459f66e90af21f55eb42f
Roger Millischer <roger.millischer@ntb.ch>
2010-09-13 11:49:23 +0000

    add method public static void multiArray()

--- jUnitTest/ch/ntb/inf/deep/testClasses/T07Arrays.java
+++ jUnitTest/ch/ntb/inf/deep/testClasses/T07Arrays.java
@@ -26,5 +26,15 @@ public class T07Arrays {
 		objs[0] = new Object();
 	}
 
+	public static void multiArray(){
+		int[][] x = new int[2][3];
+		double [][] d = new double[][]{{2.4d,3.1d},{3.5d,8.3d}};
+	}
+	
+	public static String multiObjectArray(String s){
+		Object[][] obj = new Object[3][5][6];		
+		String[][] str = new String[][]{{s,s},{s,s}};
+		return str[0][1];
+	}
 
 }
