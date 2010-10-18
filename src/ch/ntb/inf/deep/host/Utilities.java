76b60f01bd83f9535d93d84b6c6d19f25b9c926a
Ernst Dornbierer <ernst.dornbierer@ntb.ch>
2010-10-18 09:56:37 +0000

    + static void setTargetFilesFolder(String targetFilesFolder)

--- src/ch/ntb/inf/deep/host/Utilities.java
+++ src/ch/ntb/inf/deep/host/Utilities.java
@@ -1,8 +1,14 @@
 package ch.ntb.inf.deep.host;
 
 public class Utilities {
-	
+	static String targetFilesFolder = "";
+
+	public static void setTargetFilesFolder(String targetFilesFolder){
+		if(targetFilesFolder == null) targetFilesFolder = "";
+		Utilities.targetFilesFolder = targetFilesFolder;
+	}
+
 	public static String getFullClassPath(String relativeClassPath){
-		return Config.classFolder + Config.pathSeparator + relativeClassPath + Config.classFileType;
+		return targetFilesFolder + (Config.pathSeparator+ Config.classFolder + Config.pathSeparator) + relativeClassPath + Config.classFileType;
 	}
 }
