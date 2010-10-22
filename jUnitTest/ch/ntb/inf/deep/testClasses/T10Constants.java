0ab39f81df5bba00d9bbf8a603c894a70403e389
Martin Zueger <martin.zueger@ntb.ch>
2010-10-22 09:16:17 +0000

    New test class added (constants)

---
+++ jUnitTest/ch/ntb/inf/deep/testClasses/T10Constants.java
@@ -0,0 +1,8 @@
+package ch.ntb.inf.deep.testClasses;
+
+public class T10Constants {
+	public static final float f = 3.1415926535897932384626433832795f;
+	public static final double d = 2*3.1415926535897932384626433832795;
+	public static final String s1 = "HelloWorld";
+	//public static final String s2 = "äöüÄÖÜàéèê"; // TODO why does it crash when using special signs in strings?
+}
