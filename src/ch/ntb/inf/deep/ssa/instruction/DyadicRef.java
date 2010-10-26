430f879df14dbb6a84bccba074bcce0988a00d46
Urs Graf <urs.graf@ntb.ch>
2010-10-26 14:59:22 +0000

    ssa printing improved

--- src/ch/ntb/inf/deep/ssa/instruction/DyadicRef.java
+++ src/ch/ntb/inf/deep/ssa/instruction/DyadicRef.java
@@ -40,8 +40,8 @@ public class DyadicRef extends Dyadic {
 		System.out.print(" (" + result.typeName() + ")");
 		System.out.print(",   end=" + result.end);
 		if (result.index != -1) System.out.print(", index=" + result.index);
-		if (result.reg != -1) System.out.print(", reg=" + result.reg);
 		if (result.regLong != -1) System.out.print(", regLong=" + result.regLong);
+		if (result.reg != -1) System.out.print(", reg=" + result.reg);
 		if (result.join != null) System.out.print(", join={" + result.join.n + "}");
 		System.out.println();
 	}
--- src/ch/ntb/inf/deep/ssa/instruction/StoreToArray.java
+++ src/ch/ntb/inf/deep/ssa/instruction/StoreToArray.java
@@ -35,8 +35,8 @@ public class StoreToArray extends SSAInstruction {
 		System.out.print(" (" + result.typeName() + ")");
 		System.out.print(",   end=" + result.end);
 		if (result.index != -1) System.out.print(", index=" + result.index);
-		if (result.reg != -1) System.out.print(", reg=" + result.reg);
 		if (result.regLong != -1) System.out.print(", regLong=" + result.regLong);
+		if (result.reg != -1) System.out.print(", reg=" + result.reg);
 		if (result.join != null) System.out.print(", join={" + result.join.n + "}");
 		System.out.println();
 	}
