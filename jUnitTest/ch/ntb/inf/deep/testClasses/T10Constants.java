549a475bacdd51ad74c1f5a8d236a818e5f0442f
Martin Zueger <martin.zueger@ntb.ch>
2010-11-08 12:40:23 +0000

    s3 removed (comment out) because special signs (e.g. äöü) let crash the class file reader...

--- jUnitTest/ch/ntb/inf/deep/testClasses/T10Constants.java
+++ jUnitTest/ch/ntb/inf/deep/testClasses/T10Constants.java
@@ -1,8 +1,20 @@
 package ch.ntb.inf.deep.testClasses;
 
 public class T10Constants {
+	public static final boolean z = true;
+	
+	public static final byte b = 64;
+	
+	public static final short s = 256;
+	public static final char c = 'Z';
+	
+	public static final int i = 1024;
+	public static final long l = 2048;
+	
 	public static final float f = 3.1415926535897932384626433832795f;
 	public static final double d = 2*3.1415926535897932384626433832795;
-	public static final String s1 = "HelloWorld";
-	//public static final String s2 = "äöüÄÖÜàéèê"; // TODO why does it crash when using special signs in strings?
+	
+	public static final String s1 = "Hello Tester!";
+	public static final String s2 = "How are you?";
+//	public static final String s3 = "äöüÄÖÜàéèê"; // TODO why does it crash when using special signs in strings?
 }
