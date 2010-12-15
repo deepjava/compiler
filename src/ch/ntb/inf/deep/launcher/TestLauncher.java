bf2a9cc88e67cd486d2ce0e2ee0c0e11f5930a5d
Roger Millischer <roger.millischer@ntb.ch>
2010-12-15 16:13:45 +0000

    correct initRegister

--- src/ch/ntb/inf/deep/launcher/TestLauncher.java
+++ src/ch/ntb/inf/deep/launcher/TestLauncher.java
@@ -1,5 +1,6 @@
 package ch.ntb.inf.deep.launcher;
 
+
 /**
  * Launcher for tests only! Adept this file to your configuration, but don't commit
  * those changes to the SVN! Please store your project file in the top folder of
@@ -8,10 +9,30 @@ package ch.ntb.inf.deep.launcher;
  */
 public class TestLauncher {
 	public static void main(String[] args) {
-
-		Launcher.buildAll("D:/path/to/deep/project/ExampleProject.deep", "BootFromRAM");
-
-		Launcher.saveTargetImage2File("C:/temp/deep_example_target_image.dti");
+		Launcher.buildAll("D:/work/Crosssystem/deep/ExampleProject.deep", "BootFromRam");
+		//Configuration.print();
+		Launcher.downloadTargetImage();
+		//UsbMpc555Loader.getInstance();
+		//Linker.printClassList();
+		//Linker.printSystemTable();
+		//Linker.printTargetImage();
+		//Launcher.saveTargetImage2File("C:/temp/deep_example_target_image.dti");
+		
+		
+		
+//		UsbMpc555Loader.getInstance().closeConnection();
+//		USBLog log = new USBLog("UsbLog");
+//		log.addJobChangeListener(new JobChangeAdapter(){
+//			public void done(IJobChangeEvent event){
+//				if(event.getResult().isOK())
+//					System.out.println("Job completed successfully");
+//				else
+//					System.out.println("Job did not complete successfully");
+//			}
+//		});
+//		log.setSystem(true);
+//		log.schedule();
+//		while(true);
 
 	}
 
