2bc7d9c08a9e6093facea05a9c46143ef868f904
Roger Millischer <roger.millischer@ntb.ch>
2010-12-17 09:47:46 +0000

    renumbering second time

--- src/ch/ntb/inf/deep/config/ErrorCodes.java
+++ src/ch/ntb/inf/deep/config/ErrorCodes.java
@@ -3,17 +3,17 @@ package ch.ntb.inf.deep.config;
 public interface ErrorCodes {
 	
 	//Parser Error codes
-	public static final short errDigitExp = 120, errRParenExp = 121,
-	errRBraceExp = 122, errRBracketExp = 123,
-	errQuotationMarkExp = 124, errIOExp = 125,
-	errUnexpectetSymExp = 126, errLBraceExp = 127,
-	errLBracketExp = 128, errSemicolonMissExp = 129,
-	errAssignExp = 130;
+	public static final short errDigitExp = 220, errRParenExp = 221,
+	errRBraceExp = 222, errRBracketExp = 223,
+	errQuotationMarkExp = 224, errIOExp = 225,
+	errUnexpectetSymExp = 226, errLBraceExp = 227,
+	errLBracketExp = 228, errSemicolonMissExp = 229,
+	errAssignExp = 230;
 	
 	//Data tree error codes
-	public static final short errNoSuchDevice = 140, errNoDevices = 141, errSyntax = 142, errInconsistentattributes = 143,
-	errInvalidType = 144, errInvalideParameter = 145, errOverwriteProtectedConst = 146, errUndefinedConst = 147, errMaxNofReached = 148,
-	errMissingTag = 149, errNoSuchRegister = 150;
+	public static final short errNoSuchDevice = 240, errNoDevices = 241, errSyntax = 242, errInconsistentattributes = 243,
+	errInvalidType = 244, errInvalideParameter = 245, errOverwriteProtectedConst = 246, errUndefinedConst = 247, errMaxNofReached = 248,
+	errMissingTag = 249, errNoSuchRegister = 250;
 	
 	
 }
