8b247a26b4980f480c7858bbf6a26852ed19ae55
Roger Millischer <roger.millischer@ntb.ch>
2010-12-17 10:21:30 +0000

    renumbering third time, now is enough

--- src/ch/ntb/inf/deep/config/ErrorCodes.java
+++ src/ch/ntb/inf/deep/config/ErrorCodes.java
@@ -3,17 +3,17 @@ package ch.ntb.inf.deep.config;
 public interface ErrorCodes {
 	
 	//Parser Error codes
-	public static final short errDigitExp = 220, errRParenExp = 221,
-	errRBraceExp = 222, errRBracketExp = 223,
-	errQuotationMarkExp = 224, errIOExp = 225,
-	errUnexpectetSymExp = 226, errLBraceExp = 227,
-	errLBracketExp = 228, errSemicolonMissExp = 229,
-	errAssignExp = 230;
+	public static final short errDigitExp = 200, errRParenExp = 201,
+	errRBraceExp = 202, errRBracketExp = 203,
+	errQuotationMarkExp = 204, errIOExp = 205,
+	errUnexpectetSymExp = 206, errLBraceExp = 207,
+	errLBracketExp = 208, errSemicolonMissExp = 209,
+	errAssignExp = 210;
 	
 	//Data tree error codes
-	public static final short errNoSuchDevice = 240, errNoDevices = 241, errSyntax = 242, errInconsistentattributes = 243,
-	errInvalidType = 244, errInvalideParameter = 245, errOverwriteProtectedConst = 246, errUndefinedConst = 247, errMaxNofReached = 248,
-	errMissingTag = 249, errNoSuchRegister = 250;
+	public static final short errNoSuchDevice = 220, errNoDevices = 221, errSyntax = 222, errInconsistentattributes = 223,
+	errInvalidType = 224, errInvalideParameter = 225, errOverwriteProtectedConst = 226, errUndefinedConst = 227, errMaxNofReached = 228,
+	errMissingTag = 229, errNoSuchRegister = 230;
 	
 	
 }
