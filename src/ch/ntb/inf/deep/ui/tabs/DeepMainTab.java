71208c8b6ac52e971fcb6d4f864364fa81d78c7f
Roger Millischer <roger.millischer@ntb.ch>
2010-12-20 20:47:07 +0000

    make it possible to chose multiple root classes

--- src/ch/ntb/inf/deep/ui/tabs/DeepMainTab.java
+++ src/ch/ntb/inf/deep/ui/tabs/DeepMainTab.java
@@ -13,7 +13,6 @@
  *******************************************************************************/
 package ch.ntb.inf.deep.ui.tabs;
 
-
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -41,22 +40,25 @@ import org.eclipse.ui.dialogs.ResourceListSelectionDialog;
 
 import ch.ntb.inf.deep.DeepPlugin;
 
-
 /**
  * Tab to specify the Deep program to run/debug.
  */
 public class DeepMainTab extends AbstractLaunchConfigurationTab {
-	
+
 	private Text fProgramText;
 	private String locationPath;
 	private Button fProgramButton;
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#createControl(org.eclipse.swt.widgets.Composite)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.ui.ILaunchConfigurationTab#createControl(org.eclipse
+	 * .swt.widgets.Composite)
 	 */
 	public void createControl(Composite parent) {
 		Font font = parent.getFont();
-		
+
 		Composite comp = new Composite(parent, SWT.NONE);
 		setControl(comp);
 		GridLayout topLayout = new GridLayout();
@@ -64,15 +66,15 @@ public class DeepMainTab extends AbstractLaunchConfigurationTab {
 		topLayout.numColumns = 3;
 		comp.setLayout(topLayout);
 		comp.setFont(font);
-		
+
 		createVerticalSpacer(comp, 3);
-		
+
 		Label programLabel = new Label(comp, SWT.NONE);
 		programLabel.setText("&Program:");
 		GridData gd = new GridData(GridData.BEGINNING);
 		programLabel.setLayoutData(gd);
 		programLabel.setFont(font);
-		
+
 		fProgramText = new Text(comp, SWT.SINGLE | SWT.BORDER);
 		gd = new GridData(GridData.FILL_HORIZONTAL);
 		fProgramText.setLayoutData(gd);
@@ -82,52 +84,75 @@ public class DeepMainTab extends AbstractLaunchConfigurationTab {
 				updateLaunchConfigurationDialog();
 			}
 		});
-		
-		fProgramButton = createPushButton(comp, "&Browse...", null); //$NON-NLS-1$
+
+		fProgramButton = createPushButton(comp, "&Add...", null); //$NON-NLS-1$
 		fProgramButton.addSelectionListener(new SelectionAdapter() {
 			public void widgetSelected(SelectionEvent e) {
 				browseFiles();
 			}
 		});
 	}
-	
+
 	/**
-	 * Open a resource chooser to select a  program 
+	 * Open a resource chooser to select a program
 	 */
 	protected void browseFiles() {
-		ResourceListSelectionDialog dialog = new ResourceListSelectionDialog(getShell(), ResourcesPlugin.getWorkspace().getRoot(), IResource.FILE);
+		ResourceListSelectionDialog dialog = new ResourceListSelectionDialog(
+				getShell(), ResourcesPlugin.getWorkspace().getRoot(),
+				IResource.FILE);
 		dialog.setTitle("Deep Program");
 		dialog.setMessage("Select Deep Program");
 		if (dialog.open() == Window.OK) {
 			Object[] files = dialog.getResult();
 			IFile file = (IFile) files[0];
 			locationPath = file.getProject().getLocation().toString();
-			fProgramText.setText(file.getFullPath().toString());
+			String temp = fProgramText.getText();
+			if (!temp.equals("")) {
+				temp = temp + ";";
+			}
+			fProgramText.setText(temp + file.getFullPath().toString());
 		}
-		
+
 	}
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#setDefaults(org.eclipse.debug.core.ILaunchConfigurationWorkingCopy)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.ui.ILaunchConfigurationTab#setDefaults(org.eclipse.
+	 * debug.core.ILaunchConfigurationWorkingCopy)
 	 */
 	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
 	}
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#initializeFrom(org.eclipse.debug.core.ILaunchConfiguration)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.ui.ILaunchConfigurationTab#initializeFrom(org.eclipse
+	 * .debug.core.ILaunchConfiguration)
 	 */
 	public void initializeFrom(ILaunchConfiguration configuration) {
 		try {
 			String program = null;
-			program = configuration.getAttribute(DeepPlugin.ATTR_DEEP_PROGRAM, (String)null);
+			program = configuration.getAttribute(DeepPlugin.ATTR_DEEP_PROGRAM,
+					(String) null);
 			if (program != null) {
 				fProgramText.setText(program);
 			}
-			locationPath = configuration.getAttribute(DeepPlugin.ATTR_DEEP_LOCATION, "");
+			locationPath = configuration.getAttribute(
+					DeepPlugin.ATTR_DEEP_LOCATION, "");
 		} catch (CoreException e) {
 			setErrorMessage(e.getMessage());
 		}
 	}
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#performApply(org.eclipse.debug.core.ILaunchConfigurationWorkingCopy)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.ui.ILaunchConfigurationTab#performApply(org.eclipse
+	 * .debug.core.ILaunchConfigurationWorkingCopy)
 	 */
 	public void performApply(ILaunchConfigurationWorkingCopy configuration) {
 		String program = fProgramText.getText().trim();
@@ -136,39 +161,48 @@ public class DeepMainTab extends AbstractLaunchConfigurationTab {
 		}
 		configuration.setAttribute(DeepPlugin.ATTR_DEEP_PROGRAM, program);
 		configuration.setAttribute(DeepPlugin.ATTR_DEEP_LOCATION, locationPath);
-				
-		
+
 		// perform resource mapping for contextual launch
 		IResource[] resources = null;
-		if (program!= null) {
+		if (program != null) {
 			IPath path = new Path(program);
-			IResource res = ResourcesPlugin.getWorkspace().getRoot().findMember(path);
+			IResource res = ResourcesPlugin.getWorkspace().getRoot()
+					.findMember(path);
 			if (res != null) {
-				resources = new IResource[]{res};
+				resources = new IResource[] { res };
 			}
 		}
 		configuration.setMappedResources(resources);
 	}
-	
-	/* (non-Javadoc)
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#getName()
 	 */
 	public String getName() {
 		return "Main";
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#isValid(org.eclipse.debug.core.ILaunchConfiguration)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.eclipse.debug.ui.ILaunchConfigurationTab#isValid(org.eclipse.debug
+	 * .core.ILaunchConfiguration)
 	 */
 	public boolean isValid(ILaunchConfiguration launchConfig) {
 		setErrorMessage(null);
 		setMessage(null);
 		String text = fProgramText.getText();
 		if (text.length() > 0) {
-			IPath path = new Path(text);
-			if (ResourcesPlugin.getWorkspace().getRoot().findMember(path) == null) {
-				setErrorMessage("Specified program does not exist");
-				return false;
+			String[] files = text.split(";");
+			for (int i = 0; i < files.length; i++) {
+				IPath path = new Path(files[i]);
+				if (ResourcesPlugin.getWorkspace().getRoot().findMember(path) == null) {
+					setErrorMessage(files[i] + " does not exist");
+					return false;
+				}
 			}
 		} else {
 			setMessage("Specify a program");
@@ -176,10 +210,12 @@ public class DeepMainTab extends AbstractLaunchConfigurationTab {
 		return true;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.debug.ui.ILaunchConfigurationTab#getImage()
 	 */
 	public Image getImage() {
-		return null; //DebugUIPlugin.getDefault().getImageRegistry().get(DebugUIPlugin.IMG_OBJ_PDA);
+		return null; // DebugUIPlugin.getDefault().getImageRegistry().get(DebugUIPlugin.IMG_OBJ_PDA);
 	}
 }
