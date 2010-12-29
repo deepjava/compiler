11edb434601044917e28f8d332cb48697f44c51d
Urs Graf <urs.graf@ntb.ch>
2010-12-29 20:26:48 +0000

    type list corrected

--- jUnitTest/ch/ntb/inf/deep/linkerPPC/Linker00.java
+++ jUnitTest/ch/ntb/inf/deep/linkerPPC/Linker00.java
@@ -8,6 +8,7 @@ import org.junit.Test;
 import ch.ntb.inf.deep.classItems.Class;
 import ch.ntb.inf.deep.classItems.DataItem;
 import ch.ntb.inf.deep.classItems.ICclassFileConsts;
+import ch.ntb.inf.deep.classItems.Item;
 import ch.ntb.inf.deep.classItems.Method;
 import ch.ntb.inf.deep.classItems.Type;
 
@@ -26,47 +27,50 @@ public class Linker00 extends TestLinker implements ICclassFileConsts {
 			System.out.println("===================================================================");
 			System.out.println("Number of classes: " + Type.nofClasses);
 			
-			Class clazz = Type.classList;
+			Item item = Type.classList;
 			Method method;
 			DataItem field;
-			while(clazz != null) {
-				// Linker: Calculate offsets
-				Linker.calculateOffsets(clazz);
-				
-				// Print for debuging
-				System.out.println("Class: " + clazz.name.toString());
-				
-				System.out.println("  Methods:");
-				method = (Method)clazz.methods;
-				while(method  != null) {
-					System.out.println("    Name:   " + method.name.toString());
-					System.out.println("    Offset: 0x" + Integer.toHexString(method.offset) + " (" + method.offset + ")");
-					method = (Method)method.next;
-				}
-				
-				System.out.println("  Fields:");
-				field = (DataItem)clazz.fields;
-				while(field  != null) {
-					System.out.println("    Name:   " + field.name.toString());
-					System.out.println("    Offset: 0x" + Integer.toHexString(field.offset) + " (" + field.offset + ")");
-					System.out.println("    Size: " + ((Type)field.type).sizeInBits);
-					field = (DataItem)field.next;
-				}
-				
-				
-				//Linker.calculateRequiredSize(clazz);
-				//Linker.calculateAbsoluteAddresses(clazz);
-				//Linker.createConstantBlock(clazz);
-				clazz.printConstantBlock(5);
-				
-/*				System.out.println("  Constant pool:");
+			while(item != null) {
+				if( item instanceof Class){
+					Class clazz = (Class)item;
+
+					// Linker: Calculate offsets
+					Linker.calculateOffsets(clazz);
+
+					// Print for debuging
+					System.out.println("Class: " + clazz.name.toString());
+
+					System.out.println("  Methods:");
+					method = (Method)clazz.methods;
+					while(method  != null) {
+						System.out.println("    Name:   " + method.name.toString());
+						System.out.println("    Offset: 0x" + Integer.toHexString(method.offset) + " (" + method.offset + ")");
+						method = (Method)method.next;
+					}
+
+					System.out.println("  Fields:");
+					field = (DataItem)clazz.fields;
+					while(field  != null) {
+						System.out.println("    Name:   " + field.name.toString());
+						System.out.println("    Offset: 0x" + Integer.toHexString(field.offset) + " (" + field.offset + ")");
+						System.out.println("    Size: " + ((Type)field.type).sizeInBits);
+						field = (DataItem)field.next;
+					}
+
+
+					//Linker.calculateRequiredSize(clazz);
+					//Linker.calculateAbsoluteAddresses(clazz);
+					//Linker.createConstantBlock(clazz);
+					clazz.printConstantBlock(5);
+
+					/*				System.out.println("  Constant pool:");
 				System.out.println("    Size: " + clazz.constantPoolSize + " byte");
 				if(clazz.targetConstantPool != null) {
 					for(int i = 0; i < clazz.targetConstantPool.length; i++) {
 						System.out.println("    [0x" + Integer.toHexString(i * 4) + "] " + Integer.toHexString((clazz.targetConstantPool[i])));
 					}
 				}
-				
+
 				System.out.println("  String pool:");
 				System.out.println("    Size: " + clazz.stringPoolSize + " byte");
 				if(clazz.targetStringPool != null) {
@@ -74,7 +78,7 @@ public class Linker00 extends TestLinker implements ICclassFileConsts {
 						System.out.println("    [0x" + Integer.toHexString(i * 4) + "] " + Integer.toHexString((clazz.targetStringPool[i])));
 					}
 				}
-				
+
 				System.out.println("  Class descriptor:");
 				System.out.println("    Size: " + clazz.classDescriptorSize + " byte");
 				if(clazz.targetClassDescriptor != null) {
@@ -82,16 +86,17 @@ public class Linker00 extends TestLinker implements ICclassFileConsts {
 						System.out.println("    [0x" + Integer.toHexString(i * 4) + "] " + Integer.toHexString((clazz.targetClassDescriptor[i])));
 					}
 				}
-*/
-				clazz = (Class)clazz.next;
+					 */
+				}
+				item = item.next;
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-//		System.out.println(Float.toString(T10Constants.f) + " = " + Integer.toHexString(Float.floatToIntBits(T10Constants.f)));
-//		System.out.println(Double.toString(T10Constants.d) + " = " + Long.toHexString(Double.doubleToLongBits(T10Constants.d)));
+
+		//		System.out.println(Float.toString(T10Constants.f) + " = " + Integer.toHexString(Float.floatToIntBits(T10Constants.f)));
+		//		System.out.println(Double.toString(T10Constants.d) + " = " + Long.toHexString(Double.doubleToLongBits(T10Constants.d)));
 		
 	}
 	
