6ffe7a2d7380c3a1545532785d5d5038df312d73
Martin Zueger <martin.zueger@ntb.ch>
2011-01-11 16:17:18 +0000

    Testclasses update (T10 & T11)

---
+++ jUnitTest/ch/ntb/inf/deep/testClasses/T11Interfaces.java
@@ -0,0 +1,14 @@
+package ch.ntb.inf.deep.testClasses;
+
+public class T11Interfaces implements ExampleInterface{
+
+	public static double res;
+	
+	@Override
+	public void methodA() {
+		res = l + d;
+	}
+	
+	
+	
+}
