32481c02372ce4bbf8dbaaf6b066fd38ea2c1fbf
Roger Millischer <roger.millischer@ntb.ch>
2011-01-19 14:12:45 +0000

    add dbg variable and debug outprints

--- src/ch/ntb/inf/deep/ssa/SSA.java
+++ src/ch/ntb/inf/deep/ssa/SSA.java
@@ -2,16 +2,13 @@ package ch.ntb.inf.deep.ssa;
 
 import ch.ntb.inf.deep.cfg.CFG;
 import ch.ntb.inf.deep.classItems.ICclassFileConsts;
-import ch.ntb.inf.deep.config.Configuration;
-import ch.ntb.inf.deep.ssa.instruction.Monadic;
-import ch.ntb.inf.deep.ssa.instruction.PhiFunction;
-import ch.ntb.inf.deep.ssa.instruction.SSAInstruction;
 
 /**
  * @author millischer
  */
 
 public class SSA implements ICclassFileConsts, SSAInstructionOpcs {
+	private static boolean dbg = true;
 	public CFG cfg;
 	public int nofLoopheaders;
 	public boolean isParam[];
@@ -31,6 +28,7 @@ public class SSA implements ICclassFileConsts, SSAInstructionOpcs {
 		sortNodes((SSANode)cfg.rootNode);
 		determineStateArray();
 		renumberInstructions(cfg);
+		if(dbg)print(0);
 	}
 
 	public void determineStateArray() {		
@@ -232,7 +230,7 @@ public class SSA implements ICclassFileConsts, SSAInstructionOpcs {
 
 		for (int i = 0; i < level; i++)
 			System.out.print("\t");
-		System.out.println("SSA for Method: " + cfg.method.owner.name + "." + cfg.method.name);
+		System.out.println("SSA for Method: " + cfg.method.owner.name + "." + cfg.method.name + cfg.method.methDescriptor);
 		
 		SSA.renumberInstructions(cfg);
 
