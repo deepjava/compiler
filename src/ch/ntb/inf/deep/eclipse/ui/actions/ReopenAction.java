b1d81ec47890a4e1861359f334a3e7eac945ebd7
Roger Millischer <roger.millischer@ntb.ch>
2011-05-23 15:39:50 +0000

    remove target reset

--- src/ch/ntb/inf/deep/eclipse/ui/actions/ReopenAction.java
+++ src/ch/ntb/inf/deep/eclipse/ui/actions/ReopenAction.java
@@ -30,7 +30,7 @@ public class ReopenAction implements IWorkbenchWindowActionDelegate {
 		try {
 			Thread.sleep(500);//Give OS time 
 			bdi.openConnection();
-			bdi.resetTarget();
+//			bdi.resetTarget();
 			StdStreams.out.println("Device succesfully reopened");
 		} catch (DownloaderException e) {
 			bdi.closeConnection();
