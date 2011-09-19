3792fd105f03b9988fdca0148808a8557cf213ff
Martin Zueger <martin.zueger@ntb.ch>
2011-09-19 11:50:41 +0000

    Testlauncher restored. DO NOT COMMIT CHANGES TO THIS FILE!!!

--- src/ch/ntb/inf/deep/launcher/TestLauncher.java
+++ src/ch/ntb/inf/deep/launcher/TestLauncher.java
@@ -1,23 +1,21 @@
 package ch.ntb.inf.deep.launcher;
 
-import ch.ntb.inf.deep.linker.Linker32;
+import ch.ntb.inf.deep.host.ErrorReporter;
 
 /**
- * Launcher for tests only! Adept this file to your configuration, but don't commit
- * those changes to the SVN! Please store your project file in the top folder of
- * the deep-Project. You can find an example project "ExampleProject.deep" in
- * this folder which you may use as base for your own test project.
+ * Launcher for tests only! Adept this file to your configuration, but
+ * <strong>do not commit those changes to the SVN!</strong> Please store your
+ * project file in the top folder of the deep-Project. You can find an example
+ * project "ExampleProject.deep" in this folder which you may use as base for
+ * your own test project.
  */
 public class TestLauncher {
 	public static void main(String[] args) {
+		Launcher.buildAll("/PATH/TO/YOUR/PROJECT/ExampleProject.deep", "BootFromRam");
 
-		Launcher.buildAll("M:/EUser/JCC/Deep/ExampleProject.deep", "BootFromRam");
-//		Linker32.printClassList(true, false, false, true);
-		Launcher.downloadTargetImage();
-		Launcher.startTarget();
-		Launcher.saveCommandTableToFile("M:/EUser/JCC/junitTarget/tct/commandTable.dtct");
-
-		
+		if (ErrorReporter.reporter.nofErrors == 0) {
+			Launcher.downloadTargetImage();
+			Launcher.startTarget();
+		}
 	}
-
 }
