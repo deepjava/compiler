011bc21248b3ccefdcdd18722c1b9dd71707ec07
Marcel Gehrig <marcel.gehrig@ntb.ch>
2018-03-28 13:28:42 +0200

    partial merge with master

--- src/ch/ntb/inf/deep/eclipse/launcher/DeepLaunchDelegate.java
+++ src/ch/ntb/inf/deep/eclipse/launcher/DeepLaunchDelegate.java
@@ -114,7 +114,7 @@ public class DeepLaunchDelegate extends JavaLaunchDelegate{
 						Launcher.setTargetConnection(tc);
 						Launcher.openTargetConnection();
 						Launcher.downloadTargetImage();
-//						Launcher.startTarget(0x100);	// MGE
+						Launcher.startTarget(0x100);
 					} else ErrorReporter.reporter.error(812, programmer.getClassName().toString());
 				} catch (ClassNotFoundException e) {
 					ErrorReporter.reporter.error(811, programmer.getClassName().toString());
